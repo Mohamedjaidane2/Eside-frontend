55fc973193fef4ee8c98f17723a07a369dad3080
DESKTOP-IFEEQ8J\mohamed <jaidanemohammed@gmail.com>
2024-03-18 02:18:57 +0100

    create home interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from "./shared/navbar/navbar.component";
+import {CategoryBarComponent} from "./shared/category-bar/category-bar.component";
+import {HomeComponent} from "./modules/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, CategoryBarComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
