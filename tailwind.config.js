a6080b6433d38b94988abe05c5c0a317c285ce67
DESKTOP-IFEEQ8J\mohamed <jaidanemohammed@gmail.com>
2024-03-31 22:46:22 +0100

    mon compte

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,14 @@ module.exports = {
     fontFamily: {
       'sans': ['Rubi',],
     },
-    extend: {},
+    extend: {
+      backgroundImage: {
+      'light-right-arrow': "url('/assets/svg's/eside-neutral1-right-arrow.svg')",
+      'dark-right-arrow': "url('/assets/svg's/eside-neutral1-right-arrow.svg')",
+  },variants: {
+        fill: ['hover', 'focus'], // this line does the trick
+      },
+},
     colors: {
       'eside-Primary-1': '#092A49',
       'eside-Primary-2': '#3A688C',
@@ -23,6 +30,7 @@ module.exports = {
       'eside-light': 'rgba(250,187,136,0.1)',
       'eside-white': '#FFF',
       'eside-black': '#000',
+      'eside-bleu-light': '#F3F6F9',
       'eside-black-opacity-35': 'rgba(0,0,0,0.35)',
       'eside-black-opacity-70': 'rgba(0,0,0,0.7)',
       'eside-black-opacity-90': 'rgba(0,0,0,0.9)',
